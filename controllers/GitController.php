96387c7d2812a5b843192ddecc52aea91b75caf4
Zhe Xiao <zhexiao@163.com>
2015-04-21 19:34:04 -0400

    git

--- controllers/GitController.php
+++ controllers/GitController.php
@@ -8,18 +8,14 @@ class GitController extends \yii\web\Controller{
     public function actionIndex(){
         if(Yii::$app->request->isPost){
             $payload = Yii::$app->request->post('payload', '');
-            var_dump($payload);
-            if(!$payload){
-                  var_dump('there');
-                $data = json_decode($payload);
+            $data = json_decode($payload);
 
-                if($data->ref == "refs/heads/master"){          
-                    exec("git pull", $output);
-                    var_dump($output);
-                }else{
-                    echo "This branch is not master branch.";
-                }
-            }  
+            if($data->ref == "refs/heads/master"){          
+                exec("git pull", $output);
+                var_dump($output);
+            }else{
+                echo "This branch is not master branch.";
+            }
         }else{
             echo "Invalid request.";
         }
