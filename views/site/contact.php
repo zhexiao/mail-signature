9d09f0f60d65e7ec5ebcb4e4b90a64a390c9e9ee
Zhe Xiao <zhexiao@163.com>
2015-04-22 10:57:02 -0400

    add contact

--- views/site/contact.php
+++ views/site/contact.php
@@ -1,57 +1,53 @@
 <?php
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-use yii\captcha\Captcha;
-
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\ContactForm */
-
 $this->title = 'Contact';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-    <div class="alert alert-success">
-        Thank you for contacting us. We will respond to you as soon as possible.
-    </div>
-
-    <p>
-        Note that if you turn on the Yii debugger, you should be able
-        to view the mail message on the mail panel of the debugger.
-        <?php if (Yii::$app->mailer->useFileTransport): ?>
-        Because the application is in development mode, the email is not sent but saved as
-        a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-        Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-        application component to be false to enable email sending.
-        <?php endif; ?>
-    </p>
-
-    <?php else: ?>
-
-    <p>
-        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
-    </p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-                <?= $form->field($model, 'name') ?>
-                <?= $form->field($model, 'email') ?>
-                <?= $form->field($model, 'subject') ?>
-                <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
-                <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                ]) ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-
-    <?php endif; ?>
+<div class="contact-wrap">  
+    <div id="map-canvas"></div>
+    <div id="save-widget">
+       <strong>Stamford, Connecticut, USA</strong>
+       <p>Ya.. I am living at this city.</p>
+     </div>
 </div>
+
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+<script>
+    function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var myLatLng = new google.maps.LatLng(41.0982636, -73.5654152);
+        var mapOptions = {
+            center: myLatLng,
+            zoom: 10,
+        }
+
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+        var marker = new google.maps.Marker({
+            map: map,
+            // Define the place with a location, and a query string.
+            place: {
+                location: myLatLng,
+                query: 'Google, Stamford, Connecticut, USA'
+            },
+        });
+
+        // Create a new SaveWidgetOptions object for Google Sydney.
+        var saveWidgetOptions = {
+            place: {
+                location: myLatLng
+            },
+            attribution: {
+                source: 'Google Maps JavaScript API',
+                webUrl: 'https://developers.google.com/maps/'
+            }
+        };
+        var widgetDiv = document.getElementById('save-widget');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(widgetDiv);
+
+        // Append a Save Control to the existing save-widget div.
+        saveWidget = new google.maps.SaveWidget(widgetDiv, saveWidgetOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
