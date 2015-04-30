6683f7acb53ca09fa34331a58948f4e7c1c2346c
Zhe Xiao <zhexiao@163.com>
2015-04-30 18:50:25 -0400

    change git

--- controllers/GitController.php
+++ controllers/GitController.php
@@ -7,15 +7,8 @@ class GitController extends \yii\web\Controller{
 
     public function actionIndex(){
         if(Yii::$app->request->isPost){
-            $payload = Yii::$app->request->post('payload', '');
-            $data = json_decode($payload);
-
-            if($data->ref == "refs/heads/master"){          
-                exec("git pull", $output);
-                var_dump($output);
-            }else{
-                echo "This branch is not master branch.";
-            }
+            exec("git pull", $output);
+            var_dump($output);
         }else{
             echo "Invalid request.";
         }
